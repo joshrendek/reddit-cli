05a10a7e8b77094500c5b35e85656a2a1f78f50b
Josh Rendek <josh@bluescripts.net>
2012-08-22 14:01:07 -0400

    tags file and fix for testing STDOUT

--- spec/reddit_cli_spec.rb
+++ spec/reddit_cli_spec.rb
@@ -1,18 +1,25 @@
 require 'spec_helper'
+require 'stringio'
 require './lib/reddit-cli'
+
 
 describe RedditCli do 
     let(:subreddit) { "ProgrammerHumor" }
     context "#initializing" do 
+        before(:all) do 
+            $stdout = @fakeout = StringIO.new
+        end
+            
         it "should print out a story" do 
             api_response = double(RedditApi)
             api_response.stub!(:stories => 
                                [Story.new("StoryTitle", "Score", 
                                           "Comments", "URL")])
             VCR.use_cassette('reddit_programmer_humor') do 
-                STDIN.should_receive(:gets).and_return("q")
-                STDOUT.should_receive(:puts).at_least(:once)
+                $stdin.should_receive(:gets).and_return("q")
                 cli = RedditCli.new(api_response)
+                $stdout = STDOUT
+                @fakeout.string.include?('StoryTitle').should be_true
             end
         end
     end
